913070014447a9132c8ac8a0a5b52967b0442b35
hamid-bassam <hamid.bassam.147@gmail.com>
2025-11-29 14:33:31 +0100

    week 48 update

--- src/data/work/mock-work-data.ts
+++ src/data/work/mock-work-data.ts
@@ -9193,5 +9193,490 @@ export const mockWorkActivityData: WorkActivityData = JSON.parse(
       }
       // end week 15 = 47 
       ,
+      // start week 16 = 48 
+      {
+        "weekNumber": 16,
+        "startDate": "2025-10-23",
+        "endDate": "2025-10-29",
+        "weekly_insights": [
+          {
+            "totalWork": {
+              "hours": 32,
+              "minutes": 26
+            },
+            "nbDays": 5,
+            "avgPerDay": {
+              "hours": 6,
+              "minutes": 29
+            },
+            "byCategory": {
+              "avg_productive": {
+                "hours": 3,
+                "minutes": 51
+              },
+              "passive": {
+                "hours": 0,
+                "minutes": 27
+              },
+              "blocks": {
+                "hours": 2,
+                "minutes": 0
+              },
+              "ressource": {
+                "hours": 0,
+                "minutes": 10
+              }
+            },
+            "productivity_ratio": 59.46
+          }
+        ],
+        "daily_insights": [
+          {
+            "day": "jeudi 23/10",
+            "totalWork": {
+              "hours": 3,
+              "minutes": 5
+            },
+            "byCategory": {
+              "productive": {
+                "hours": 3,
+                "minutes": 5
+              },
+              "passive": {
+                "hours": 0,
+                "minutes": 0
+              },
+              "blocks": {
+                "hours": 0,
+                "minutes": 0
+              },
+              "ressource": {
+                "hours": 0,
+                "minutes": 0
+              }
+            },
+            "entry": "09:45",
+            "exit": "16:15",
+            "presence": {
+              "hours": 6,
+              "minutes": 30
+            }
+          },
+          {
+            "day": "ven 24/10",
+            "totalWork": {
+              "hours": 2,
+              "minutes": 20
+            },
+            "byCategory": {
+              "productive": {
+                "hours": 2,
+                "minutes": 10
+              },
+              "passive": {
+                "hours": 0,
+                "minutes": 0
+              },
+              "blocks": {
+                "hours": 0,
+                "minutes": 5
+              },
+              "ressource": {
+                "hours": 0,
+                "minutes": 5
+              }
+            },
+            "entry": "10:00",
+            "exit": "15:15",
+            "presence": {
+              "hours": 5,
+              "minutes": 15
+            }
+          },
+          {
+            "day": "dim",
+            "totalWork": {
+              "hours": 16,
+              "minutes": 9
+            },
+            "byCategory": {
+              "productive": {
+                "hours": 3,
+                "minutes": 55
+              },
+              "passive": {
+                "hours": 2,
+                "minutes": 17
+              },
+              "blocks": {
+                "hours": 9,
+                "minutes": 52
+              },
+              "ressource": {
+                "hours": 0,
+                "minutes": 5
+              }
+            },
+            "entry": "00:50",
+            "exit": "18:00",
+            "presence": {
+              "hours": 17,
+              "minutes": 10
+            }
+          },
+          {
+            "day": "mar 28/10",
+            "totalWork": {
+              "hours": 5,
+              "minutes": 2
+            },
+            "byCategory": {
+              "productive": {
+                "hours": 4,
+                "minutes": 22
+              },
+              "passive": {
+                "hours": 0,
+                "minutes": 0
+              },
+              "blocks": {
+                "hours": 0,
+                "minutes": 0
+              },
+              "ressource": {
+                "hours": 0,
+                "minutes": 40
+              }
+            },
+            "entry": "09:20",
+            "exit": "18:40",
+            "presence": {
+              "hours": 9,
+              "minutes": 20
+            }
+          },
+          {
+            "day": "mer 29/10",
+            "totalWork": {
+              "hours": 5,
+              "minutes": 50
+            },
+            "byCategory": {
+              "productive": {
+                "hours": 5,
+                "minutes": 45
+              },
+              "passive": {
+                "hours": 0,
+                "minutes": 0
+              },
+              "blocks": {
+                "hours": 0,
+                "minutes": 5
+              },
+              "ressource": {
+                "hours": 0,
+                "minutes": 0
+              }
+            },
+            "entry": "09:50",
+            "exit": "18:35",
+            "presence": {
+              "hours": 8,
+              "minutes": 45
+            }
+          }
+        ],
+        "activities": [
+          {
+            "name": "acccdp",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 6
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "Christine + nourhan",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "avisexpert",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 27
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "Tracking avisexpert – productive time.",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "avisexperts",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 30
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "Tracking avisexperts – productive time.",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "br",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 2
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "ressource",
+            "description": "café",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "check",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 29
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "Tracking check – productive time.",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "daily",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 12
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "Tracking daily – productive time.",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "debrief",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 4
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "thierry",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "direct",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 8
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "ressource",
+            "description": "gilles",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "manager",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 5
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "Tracking manager – productive time.",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "off",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 11
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "Thierry",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "orgadocs",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 6
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "Tracking orgadocs – productive time.",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "pegase",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 24
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "kpi #team",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "prep",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 5
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "Tracking prep – productive time.",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "prepcap",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 6
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "dark #jacques",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "resp",
+            "trackedAvgPerDay": {
+              "hours": 2,
+              "minutes": 0
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "blocks",
+            "description": "café",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "roadmap",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 8
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "thierry",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "strat",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 47
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "process #boss",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "tw",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 27
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "passive",
+            "description": "Tracking tw – passive time.",
+            "icon": "activity",
+            "impressions": []
+          },
+          {
+            "name": "work",
+            "trackedAvgPerDay": {
+              "hours": 0,
+              "minutes": 9
+            },
+            "goalAvgPerDay": {
+              "hours": 0,
+              "minutes": 0
+            },
+            "type": "productive",
+            "description": "$",
+            "icon": "activity",
+            "impressions": []
+          }
+        ],
+        "globalRate": -1
+      }
+      // end week 16 = 48 
+      ,
     ]
   ));
